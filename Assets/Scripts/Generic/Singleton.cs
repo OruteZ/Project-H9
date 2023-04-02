8a7cfda5c449bab6c0cca09b44d07f2ee8420019
OruteZ <creeperss85@gmail.com>
2023-04-02 23:03:27 +0900

    Singleton 제네릭 클래스 생성

---
+++ Assets/Scripts/Generic/Singleton.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Generic
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+            
+                //없으면 동일 타입의 오브젝트 탐색
+                _instance = (T)FindObjectOfType(typeof(T));
+
+                if (_instance != null) return _instance;
+            
+                //찾아도 없으면 오브젝트 생성
+                var obj = new GameObject(typeof(T).Name, typeof(T));
+                _instance = obj.GetComponent<T>();
+
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (transform.parent != null && transform.root != null)
+            {
+                DontDestroyOnLoad(transform.root.gameObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}

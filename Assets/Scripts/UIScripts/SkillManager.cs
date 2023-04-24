757161d1f019b26260a3bafa193468f274af399d
MtNight <eskwon0228@gmail.com>
2023-04-24 17:12:57 +0900

    스킬 클래스 생성

--- Assets/Scripts/UIScripts/SkillManager.cs
+++ Assets/Scripts/UIScripts/SkillManager.cs
@@ -19,24 +19,20 @@ public class Skill
     private int range { get; set; }
     private int width { get; set; }
 
-    //public static class SKillBuilder
-    //{
-    //    private int index;
-    //    private string _icon;
-    //    private string _name;
-    //    private string _description;
-    //    public SKillBuilder(int index, string _icon)
-    //    {
-    //        this._index = index;
-    //        this._icon = _icon;
-    //    }
-    //}
     public Skill(List<string> _list) 
     {
         InitValues(_list);
     }
     private void InitValues(List<string> _list) 
     {
+        for (int i = 0; i < _list.Count; i++) 
+        {
+            if (_list[i].Equals("")) 
+            {
+                _list[i] = "0";
+            }
+        }
+
         index = int.Parse(_list[0]);
         icon = _list[1];
         name = _list[2];
@@ -46,7 +42,6 @@ public class Skill
         preced = InitIntArrayValue(_list[6]);
         repeat = int.Parse(_list[7]);
         stat = InitIntArrayValue(_list[8]);
-        Debug.Log(_list[9]);
         activeType = int.Parse(_list[9]);
         upgSkill = int.Parse(_list[10]);
         amount = InitIntArrayValue(_list[11]);
@@ -93,24 +88,24 @@ public class SkillManager : MonoBehaviour
 
     void SetSkills()
     {
-        //skillTable = SkillRead.Read("SkillTable");
-        //if (skillTable == null)
-        //{
-        //    Debug.Log("skill table을 읽어오지 못했습니다.");
-        //}
+        skillTable = SkillRead.Read("SkillTable");
+        if (skillTable == null)
+        {
+            Debug.Log("skill table을 읽어오지 못했습니다.");
+        }
 
-        //skills = new List<Skill>();
-        //for (int i = 0; i < skillTable.Count; i++) 
-        //{
-        //    Skill skill = new Skill(skillTable[i]);
-        //    skills.Add(skill);
-        //}
+        skills = new List<Skill>();
+        for (int i = 0; i < skillTable.Count; i++)
+        {
+            Skill skill = new Skill(skillTable[i]);
+            skills.Add(skill);
+        }
 
 
-        //for (int i = 0; i < skills.Count; i++) 
-        //{
-        //    Debug.Log(skills[i].index);
-            
-        //}
+        for (int i = 0; i < skills.Count; i++)
+        {
+            Debug.Log(skills[i].index);
+
+        }
     }
 }

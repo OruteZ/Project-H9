006a0ea1e7c14f76e6fc087689fc3d97824eb2ba
MtNight <eskwon0228@gmail.com>
2023-04-24 16:50:07 +0900

    스킬테이블 파일 읽어오기 추가

--- Assets/Scripts/UIScripts/SkillManager.cs
+++ Assets/Scripts/UIScripts/SkillManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-class Skill
+public class Skill
 {
-    private int index {get; set;}
+    public int index {get; set;}
     private string icon { get; set; }
     private string name { get; set; }
     private string description { get; set; }
@@ -19,31 +19,52 @@ class Skill
     private int range { get; set; }
     private int width { get; set; }
 
+    //public static class SKillBuilder
+    //{
+    //    private int index;
+    //    private string _icon;
+    //    private string _name;
+    //    private string _description;
+    //    public SKillBuilder(int index, string _icon)
+    //    {
+    //        this._index = index;
+    //        this._icon = _icon;
+    //    }
+    //}
     public Skill(List<string> _list) 
     {
         InitValues(_list);
     }
-
     private void InitValues(List<string> _list) 
     {
         index = int.Parse(_list[0]);
         icon = _list[1];
         name = _list[2];
         description = _list[3];
-        category = int.Parse(_list[0]);
-        aop = int.Parse(_list[0]);
-        preced = InitIntArrayValue(_list[0]);
-        repeat = int.Parse(_list[0]);
-        stat = InitIntArrayValue(_list[0]);
-        activeType = int.Parse(_list[0]);
-        upgSkill = int.Parse(_list[0]);
-        amount = InitIntArrayValue(_list[0]);
-        range = int.Parse(_list[0]);
-        width = int.Parse(_list[0]);
+        category = int.Parse(_list[4]);
+        aop = int.Parse(_list[5]);
+        preced = InitIntArrayValue(_list[6]);
+        repeat = int.Parse(_list[7]);
+        stat = InitIntArrayValue(_list[8]);
+        Debug.Log(_list[9]);
+        activeType = int.Parse(_list[9]);
+        upgSkill = int.Parse(_list[10]);
+        amount = InitIntArrayValue(_list[11]);
+        range = int.Parse(_list[12]);
+        width = int.Parse(_list[13]);
     }
     private int[] InitIntArrayValue(string _string) 
     {
-        int[] result = { 0 };
+        if (_string.Length == 0) return new int[] { 0 };
+
+        string[] splitString = _string.Split('$');
+
+        int[] result = new int[splitString.Length];
+
+        for (int i = 0; i < splitString.Length; i++) 
+        {
+            result[i] = int.Parse(splitString[i]);
+        }
 
         return result;
     } 
@@ -51,15 +72,45 @@ class Skill
 
 public class SkillManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private List<List<string>> skillTable;
+    private List<Skill> skills;
+
+    private void Awake()
+    {
+        SetSkills();
+
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetSkills()
+    {
+        //skillTable = SkillRead.Read("SkillTable");
+        //if (skillTable == null)
+        //{
+        //    Debug.Log("skill table을 읽어오지 못했습니다.");
+        //}
+
+        //skills = new List<Skill>();
+        //for (int i = 0; i < skillTable.Count; i++) 
+        //{
+        //    Skill skill = new Skill(skillTable[i]);
+        //    skills.Add(skill);
+        //}
+
+
+        //for (int i = 0; i < skills.Count; i++) 
+        //{
+        //    Debug.Log(skills[i].index);
+            
+        //}
     }
 }

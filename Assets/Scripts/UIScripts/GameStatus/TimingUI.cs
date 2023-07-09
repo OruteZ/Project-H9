e40882e3edf7c8e3ca67df9c300d74d9455df698
MtNight <eskwon0228@gmail.com>
2023-07-09 23:52:20 +0900

    턴 표시 UI 추가 - 테스트 필요

--- Assets/Scripts/UIScripts/GameStatus/TimingUI.cs
+++ Assets/Scripts/UIScripts/GameStatus/TimingUI.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimingUI : UISystem
 {
+    [SerializeField] private GameObject TurnText;
+
+    public TurnSystem turnSystem;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,12 @@ public class TimingUI : UISystem
     // Update is called once per frame
     void Update()
     {
+        SetTurnText(turnSystem.turnNumber);
+    }
 
+    public void SetTurnText(int currentTurn) 
+    {
+        TurnText.GetComponent<TextMeshProUGUI>().text = "Current Turn: " + currentTurn.ToString();
     }
 
     public override void OpenUI()

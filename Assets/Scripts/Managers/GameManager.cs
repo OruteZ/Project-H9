6b97c0ff330454d0deacd783b64097c81afa592d
MtNight <eskwon0228@gmail.com>
2023-10-12 22:59:09 +0900

    스킬 UI에서 테스트용 스킬 습득 시 실제 효과가 반영되어 적용되는 기능 추가

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -92,4 +92,26 @@ public class GameManager : Generic.Singleton<GameManager>
         _currentState = state;
         //UIManager.instance.ChangeScenePrepare(state);
     }
+
+    public void AddPlayerSkillListElement(int skillIndex, bool isPassive) 
+    {
+        List<int> list = null;
+        if (isPassive) 
+        {
+            list = playerPassiveIndexList;
+        }
+        else
+        {
+            list = playerActiveIndexList;
+        }
+        foreach (int i in list) 
+        {
+            if (i == skillIndex) 
+            {
+                Debug.Log("동일한 스킬 연속 습득 오류");
+                return;
+            }
+        }
+        list.Add(skillIndex);
+    }
 }

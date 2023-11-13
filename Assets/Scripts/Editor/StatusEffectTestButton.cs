b38be9d240cba82b9485d6ef897bc267b7a66000
OZ <dwd0102@sju.ac.kr>
2023-11-13 17:55:06 +0900

    StatusEffectTestButton

---
+++ Assets/Scripts/Editor/StatusEffectTestButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(StatusEffectTester))]
+public class StatusEffectTestButton : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        StatusEffectTester tester = (StatusEffectTester)target;
+        
+        if (GUILayout.Button("Effect Player") && EditorApplication.isPlaying)
+        {
+            tester.Effect();
+        }
+        
+        if (GUILayout.Button("Debug Effect") && EditorApplication.isPlaying)
+        {
+            //get all status from player
+            //print status
+            var player = FieldSystem.unitSystem.GetPlayer();
+            
+            //get all displayable status
+            var statusList = player.GetDisplayableEffects();
+            
+            //print displayable status's stack, duration, name if canDisplay
+            foreach (var status in statusList)
+            {
+                if (status.CanDisplay() is false) continue;
+                
+                Debug.Log($"{status.GetName()} : {status.GetStack()} : {status.GetDuration()}");
+            }
+        }
+    }
+}

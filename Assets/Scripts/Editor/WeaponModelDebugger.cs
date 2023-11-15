7945755216adf479c215eca258d5ea7239f44418
OZ <dwd0102@sju.ac.kr>
2023-11-15 14:54:29 +0900

    무장해제 상태이상 애니메이션 수정

--- Assets/Scripts/Editor/WeaponModelDebugger.cs
+++ Assets/Scripts/Editor/WeaponModelDebugger.cs
@@ -2,20 +2,20 @@
 using UnityEditor;
 
     //class weapon model's editor
-    [CustomEditor(typeof(WeaponModel))]
-    public class WeaponModelDebugger : UnityEditor.Editor
+[CustomEditor(typeof(WeaponModel))]
+public class WeaponModelDebugger : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
     {
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-            
-            //get weapon model
-            WeaponModel weaponModel = (WeaponModel)target;
-            
-            //if weapon model is null, return
-            if (weaponModel == null) return;
-            
-            //if weapon model is not null, show weaponmodel's isVisible value
-            EditorGUILayout.LabelField("isVisible", weaponModel.isVisible.ToString());
-        }
+        base.OnInspectorGUI();
+        
+        //get weapon model
+        WeaponModel weaponModel = (WeaponModel)target;
+        
+        //if weapon model is null, return
+        if (weaponModel == null) return;
+        
+        //if weapon model is not null, show weaponmodel's isVisible value
+        EditorGUILayout.LabelField("isVisible", weaponModel.isVisible.ToString());
+    }
 }

955badd5cd443192023da1caac6f50461f08c869
OZ <dwd0102@sju.ac.kr>
2024-01-30 08:26:48 +0900

    IItemAttribute.cs 추가

--- Assets/Scripts/Inventory/interface/IItemAttribute.cs
+++ Assets/Scripts/Inventory/interface/IItemAttribute.cs
@@ -1,4 +1,17 @@
 public interface IItemAttribute
 {
+    /// <summary>
+    /// 특성의 고유 Index를 가져옵니다.
+    /// </summary>
+    public int GetIndex();
     
+    /// <summary>
+    /// 특성의 이름을 가져옵니다.
+    /// </summary>
+    public string GetName();
+    
+    /// <summary>
+    /// 특성의 설명을 가져옵니다.
+    /// </summary>
+    public string GetDescription();
 }

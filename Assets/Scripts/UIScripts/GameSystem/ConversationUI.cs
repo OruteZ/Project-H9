f11a855a448fabc7cde159e9a96c309713d3bd66
MtNight <eskwon0228@gmail.com>
2024-04-02 22:10:03 +0900

    대화창 UI 레이아웃 추가 및 액션 아이콘 변경

---
+++ Assets/Scripts/UIScripts/GameSystem/ConversationUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationUI : UISystem
+{
+    [SerializeField] private GameObject _conversationWindow;
+    [SerializeField] private GameObject _speakerText;
+    [SerializeField] private GameObject _contentsText;
+
+    public void StartConversation(int group) 
+    {
+
+    }
+}

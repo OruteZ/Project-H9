911cbbcdb3814b787ccc724d6a6d6913def38bb3
Moonsh <kimgaejin@naver.com>
2024-04-18 14:08:36 +0900

    bugfix) 퀘스트, IN_SIGHT 관련하여 작동이 안되는 버그 수정

--- Assets/Scripts/Events/PlayerEvents.cs
+++ Assets/Scripts/Events/PlayerEvents.cs
@@ -2,6 +2,6 @@ using UnityEngine.Events;
 
 public static class PlayerEvents
 {
-    public static UnityEvent<Link> OnEnteredLinkinSight;
-    public static UnityEvent<Tile> OnEnteredTileinSight;
+    public static UnityEvent<Link> OnEnteredLinkinSight = new UnityEvent<Link>();
+    public static UnityEvent<Tile> OnEnteredTileinSight = new UnityEvent<Tile>();
 }

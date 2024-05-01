423095a75c21deb9ac626b4ad0b22f00373d9a61
OZ <dwd0102@sju.ac.kr>
2024-05-01 14:16:50 +0900

    AnimationEvent Setting

---
+++ Assets/Scripts/Utility/AnimationEvent.cs
@@ -0,0 +1,16 @@
+public static class AnimationEventNames
+{
+    public const string GUN_FIRE = "GunFire";
+    public const string GUN_RELOAD = "GunReload";
+    public const string GUN_RELOAD_END = "GunReloadEnd";
+    
+    public static bool IsEventName(string eventName)
+    {
+        return eventName 
+            is GUN_FIRE 
+            or GUN_RELOAD 
+            or GUN_RELOAD_END
+            
+       ;
+    }
+}

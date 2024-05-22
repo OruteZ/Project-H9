2b0eee1b305f5952af2c2893f7fe36c83a8c70df
OZ <dwd0102@sju.ac.kr>
2024-05-23 01:03:53 +0900

    pause Menu 프리팹화 + 기능 추가

--- Assets/Scripts/UIScripts/PauseWindow/PauseMenuUI.cs
+++ Assets/Scripts/UIScripts/PauseWindow/PauseMenuUI.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 메뉴 버튼을 눌렀을 때 표시할 각종 UI를 관리하는 클래스
 /// </summary>
 public class PauseMenuUI : UISystem
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     public void OnResumeBtnClick()
     {
         UIManager.instance.SetUILayer(1);
+    }
+    
+    public void BackToTitle()
+    {
+        //Find all GameObjects with DontDestroyOnLoad
+        foreach (GameObject obj in FindObjectsOfType<GameObject>())
+        {
+            if (obj.scene.name == null)
+            {
+                // Destroy the GameObject
+                Destroy(obj);
+            }
+        }
+        
+        SceneManager.LoadScene($"TitleScene");
+    }
+    
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        
+        #else
+        Application.Quit();
+        
+        #endif
     }
 }

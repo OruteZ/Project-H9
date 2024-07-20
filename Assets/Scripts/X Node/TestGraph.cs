17037f0b25f9df02535080ba8b53e4150b90ce90
OZ <dwd0102@sju.ac.kr>
2024-07-20 19:55:31 +0900

    Remove X Node

--- Assets/Scripts/X Node/TestGraph.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using XNode;
-
-[CreateAssetMenu(fileName = "TestGraph", menuName = "TestGraph", order = 0)]
-public class TestGraph : NodeGraph { 
-	
-}
